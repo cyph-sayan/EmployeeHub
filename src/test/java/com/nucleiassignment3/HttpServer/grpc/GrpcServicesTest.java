79ccf816164d2151e8d3ed0428e3b21ab395b91b
cyph-sayan <sayan.manna.11.8.2000@gmail.com>
2023-02-24 15:47:09 +0530

    added the test for all the grpc services

--- src/test/java/com/nucleiassignment3/HttpServer/grpc/GrpcServicesTest.java
+++ src/test/java/com/nucleiassignment3/HttpServer/grpc/GrpcServicesTest.java
@@ -1,4 +1,4 @@
-package grpc;
+package com.nucleiassignment3.HttpServer.grpc;
 
 import com.mysql_crud.grpc.*;
 import io.grpc.ManagedChannel;
@@ -46,6 +46,38 @@ public class GrpcServicesTest {
         assertEquals(11,getEmployeeResponse.getEmployee().getDob().getDay());
         assertEquals(6,getEmployeeResponse.getEmployee().getDob().getMonth());
         assertEquals(2000,getEmployeeResponse.getEmployee().getDob().getYear());
+        channel.shutdown();
+    }
+    @Test
+    void updateEmployee(){
+        ManagedChannel managedChannel=ManagedChannelBuilder.forAddress("localhost",8080)
+                .usePlaintext()
+                .build();
+        EmployeesServiceGrpc.EmployeesServiceBlockingStub stub=EmployeesServiceGrpc.newBlockingStub(managedChannel);
+        UpdateEmployeeResponse updateEmployeeResponse=stub.updateEmployee(UpdateEmployeeRequest.newBuilder()
+                .setEmployee(Employee.newBuilder().setId("GN-ZVC95SR2KDNB70WVALZRZ").setName("RamPrasad").setDob(Date.newBuilder().setDay(11).setMonth(8).setYear(2000).build()).setGender("M").build())
+                .build());
+        assertEquals("GN-ZVC95SR2KDNB70WVALZRZ", updateEmployeeResponse.getEmployee().getId());
+        assertEquals("RamPrasad",updateEmployeeResponse.getEmployee().getName());
+        assertEquals("M",updateEmployeeResponse.getEmployee().getGender());
+        assertEquals(11,updateEmployeeResponse.getEmployee().getDob().getDay());
+        assertEquals(8,updateEmployeeResponse.getEmployee().getDob().getMonth());
+        assertEquals(2000,updateEmployeeResponse.getEmployee().getDob().getYear());
+        managedChannel.shutdown();
+    }
+    @Test
+    void listEmployees()
+    {
+        ManagedChannel channel= ManagedChannelBuilder.forAddress("localhost",8080)
+                .usePlaintext()
+                .build();
+        EmployeesServiceGrpc.EmployeesServiceBlockingStub stub=EmployeesServiceGrpc.newBlockingStub(channel);
+        ListEmployeesResponse listEmployeesResponse=stub.listEmployees(ListEmployeesRequest
+                .newBuilder()
+                .setPageNumber(0)
+                .setPageSize(10)
+                .build());
+        assertEquals(2,listEmployeesResponse.getEmployeesList().size());
         channel.shutdown();
     }
 }
